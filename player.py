d3174008984dc546c48e8b489b59d942ca5a11d4
ka-lu <kml43@sfu.ca>
2025-03-08 11:10:58 -0800

    created methods in player class

--- player.py
+++ player.py
@@ -9,13 +9,27 @@ class Player(pygame.sprite.sprite):
         self.x_velocity = 0
         self.y_velocity = 0
         self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
 
     def move(self, dx,dy):
         self.rect.x_coord += dx
         self.rect.y += dy
 
     def move_left (self, velocity):
-        self.x_velocity = -vel
+        self.x_velocity = -vel       
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count =0
 
     def move_right (self,velcoity):
-        self.x_velocity = +vel
+        self.x_velocity = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count =0
+
+    def loop(self,fps):
+        self.move(self.x_velocity, self.y_velocity)
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
