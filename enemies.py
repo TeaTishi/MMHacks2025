202a0267b193794c7ab845199a529083c2c9cd67
Tiensa Tran <tiensat7@gmail.com>
2025-03-08 11:42:30 -0800

    enemies

--- enemies.py
+++ enemies.py
@@ -1,2 +1,17 @@
 import pygame
 
+class enemies(object):
+
+    walkRight = [pygame.image.load('rightrat.png')]
+    walkLeft = [pygame.image.load('leftrat.png')]
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.path = [x, end]
+        self.walkCount = 0
+        self.velocity = 3
+
+    def draw(self, screen):
+        screen.blit(pygame.image.load('rightrat.png'), (self.x, self.y))
