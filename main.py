3fd23999959558fb784118a28c70ddbe6a64e844
Tiensa Tran <tiensat7@gmail.com>
2025-03-08 11:17:17 -0800

    clock

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 WIDTH = 1920
 HEIGHT = 1080
 background_colour = (234, 212, 252) 
+
+FPS = 60
   
 screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
   
@@ -13,12 +15,16 @@ screen.fill(background_colour)
 
 pygame.display.flip() 
 
-def get_background(name):
-    image = pygame.image.load(join("assests", "Background", name))
+# def get_background(name):
+    # image = pygame.image.load(join("assests", name))
 
-def main(screen):
-    running = True
-    while running: 
-        for event in pygame.event.get():            
-            if event.type == pygame.QUIT: 
-                running = False
+clock = pygame.time.Clock()
+
+running = True
+while running: 
+    for event in pygame.event.get():            
+        if event.type == pygame.QUIT: 
+            running = False
+    
+    pygame.display.update()
+    clock.tick(FPS)
