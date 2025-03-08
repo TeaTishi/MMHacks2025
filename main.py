c32c50f2a5c1590579885e0ebb45115415dc2f45
ka-lu <kml43@sfu.ca>
2025-03-08 11:42:27 -0800

    restarted the player class

--- main.py
+++ main.py
@@ -18,10 +18,33 @@ def get_background(name):
 
 def main(screen):
 
-    #player = Player(100,100,50,50)
+
 
     running = True
     while running: 
         for event in pygame.event.get():            
             if event.type == pygame.QUIT: 
                 running = False
+
+
+# def main (window):
+#     clock = pygame.time.Clock()
+#     background, bg_image = get_background("background.jpg")
+    
+#     player = Player (100,100,50,50)
+
+#     run = True
+#     while run:
+#         clock.tick(FPS)
+
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 run = False
+#                 break
+#         draw(window, background, bg_image, player)
+
+#     pygame.quit()
+#     quit()
+
+# if __name__ == "__main__":
+#     main(window)
