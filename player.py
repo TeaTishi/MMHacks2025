65e4432118229b5885794cc318ba37f50721a836
ka-lu <kml43@sfu.ca>
2025-03-08 10:44:33 -0800

    added some methods to Player class

--- player.py
+++ player.py
@@ -1,3 +1,21 @@
 
 x_coord =0
 y_coord =0
+
+class Player(pygame.sprite.sprite):
+    COLOR = (255, 0,0)
+    def __init__ (self, x_coord, y_coord, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_velocity = 0
+        self.y_velocity = 0
+        self.mask = None
+
+    def move(self, dx,dy):
+        self.rect.x_coord += dx
+        self.rect.y += dy
+
+    def move_left (self, velocity):
+        self.x_velocity = -vel
+
+    def move_right (self,velcoity):
+        self.x_velocity = +vel
